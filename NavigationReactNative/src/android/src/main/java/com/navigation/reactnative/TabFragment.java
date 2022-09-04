835d49a772d9385ffa7221e0bd66e7fd6c602ac4
Graham Mendick <graham.mendick@gmail.com>
2022-09-04 13:37:04 +0100

    Updated view on create
    First time in the content is null because it's frozen. So need to read the latest content when create is called again - this happens after switching to the tab. It's too late to fix it for first switch but it does work for subsequent switches - because it's now already updated from first switch

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabFragment.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabFragment.java
@@ -26,6 +26,7 @@ public class TabFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = tabBarItem.content.size() > 0 ? tabBarItem.content.get(0).content : null;
         return view != null ? view : new View(getContext());
     }
 }
